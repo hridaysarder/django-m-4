2368922a233663ed44c533fe0adfff332f905f2c
hridaysarder <hridoyaug12@gmail.com>
2025-02-02 14:44:54 +0600

    Added users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from users.forms import RegisterForm,CustomRegistrationForm
 # Create your views here.
+def sign_up(request):
+    if request.method == 'GET':
+        form=CustomRegistrationForm()
+    if request.method == 'POST':
+        form=CustomRegistrationForm(request.POST)
+        if form.is_valid():
+
+            # username=form.cleaned_data.get('username')
+            # password=form.cleaned_data.get('password')
+            # confirm_password=form.cleaned_data.get('confirm_password')
+            # if password==confirm_password:
+            #     User.objects.create(username=username,password=password)
+            # else:
+            #     print('Password are not same')
+            form.save()
+        else:
+            print("Form is not valid")
+    return render(request,'registration/register.html',{"form":form})
