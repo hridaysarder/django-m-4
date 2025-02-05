06887bd94526beb40b6a342f6343891e29cd4709
hridaysarder <hridoyaug12@gmail.com>
2025-02-05 23:48:51 +0600

    Added admin dashboard

--- tasks/forms.py
+++ tasks/forms.py
@@ -19,12 +19,13 @@ class TaskForm(forms.Form):
 
 
 class StyledFormMixin:
+    """ Mixing to apply style to form field"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.apply_styled_widgets() 
+    def __init__(self, *arg, **kwarg):
+        super().__init__(*arg, **kwarg)
+        self.apply_styled_widgets()
 
-    default_classes = "border-2 border-gray-300 w-full p-3 rounded-lg shadow-sm focus:outline-none focus:border-blue-500 focus:ring-blue-500"
+    default_classes = "border-2 border-gray-300 w-full p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
 
     def apply_styled_widgets(self):
         for field_name, field in self.fields.items():
@@ -36,9 +37,25 @@ class StyledFormMixin:
             elif isinstance(field.widget, forms.Textarea):
                 field.widget.attrs.update({
                     'class': f"{self.default_classes} resize-none",
-                    'placeholder': f"Enter {field.label.lower()}",
-                    'rows': 4
+                    'placeholder':  f"Enter {field.label.lower()}",
+                    'rows': 5
                 })
+            elif isinstance(field.widget, forms.SelectDateWidget):
+                # print("Inside Date")
+                field.widget.attrs.update({
+                    "class": "border-2 border-gray-300 p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
+                })
+            elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+                # print("Inside checkbox")
+                field.widget.attrs.update({
+                    'class': "space-y-2"
+                })
+            else:
+                # print("Inside else")
+                field.widget.attrs.update({
+                    'class': self.default_classes
+                })
+
 
 
 class TaskModelForm(StyledFormMixin, forms.ModelForm):
@@ -46,9 +63,9 @@ class TaskModelForm(StyledFormMixin, forms.ModelForm):
         model = Task
         fields = ['title', 'description', 'due_date', 'assigned_to']
 
-        widgets={
-            'due_date':forms.SelectDateWidget,
-            'assigned_to':forms.CheckboxSelectMultiple
+        widgets = {
+            'due_date': forms.SelectDateWidget,
+            'assigned_to': forms.CheckboxSelectMultiple
         }
 
 
@@ -56,5 +73,3 @@ class TaskDetailModelForm(StyledFormMixin, forms.ModelForm):
     class Meta:
         model = TaskDetail
         fields = ['priority', 'notes']
-
-   
