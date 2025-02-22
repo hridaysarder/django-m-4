40d561b14b389929210101fac22192510b56ca14
hridaysarder <hridoyaug12@gmail.com>
2025-02-23 00:36:12 +0600

    Added class base view create_task and view_task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from tasks.views import manager_dashboard,employee_dashboard,create_task,view_task,update_task,delete_task,task_details,dashboard
+from tasks.views import manager_dashboard,employee_dashboard,view_task,update_task,delete_task,task_details,dashboard,Greetings,HiGreetings,CreateTask,ViewProject
 
 
 urlpatterns = [
   path('manager-dashboard/',manager_dashboard,name='manager-dashboard'),
   path('user-dashboard/',employee_dashboard,name='user-dashboard'),
-  path('create-task/',create_task,name='create-task'),
-  path('view_task/',view_task),
+  # path('create-task/',create_task,name='create-task'),
+  path('view-task/',ViewProject.as_view(),name='view-task'),
   path('task/<int:task_id>/details/',task_details,name='task-details'),
   path('update-task/<int:id>/',update_task,name='update-task'),
   path('delete-task/<int:id>/',delete_task,name='delete-task'),
-  path('dashboard',dashboard,name='dashboard')
+  path('dashboard',dashboard,name='dashboard'),
+  path('greetings/',Greetings.as_view(),name='greetings'),
+  path('higreetings/',HiGreetings.as_view(),name='higreetings'),
+  path('create-task/',CreateTask.as_view(),name='create-task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from tasks.forms import TaskModelForm, TaskDetailModelForm
 from tasks.models import Task,Project
 from django.db.models import Count, Q
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required,user_passes_test,permission_required
 from users.views import is_admin
+from django.http import HttpResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic.base import ContextMixin
+from django.views.generic import ListView
 
+
+
+# class base view
+class Greetings(View):
+    greetings='Hello World'
+    
+    def get(self,request):
+        return HttpResponse(self.greetings)
+    
+class HiGreetings(Greetings):
+    greetings='Hi, How are you'
 
 def is_manager(user):
     return user.groups.filter(name='Manager').exists()
@@ -81,6 +98,45 @@ def create_task(request):
     context = {"task_form": task_form, "task_detail_form": task_detail_form}
     return render(request, "task_form.html", context)
 
+# vaiable for list of decorators
+create_decorators=[login_required,permission_required('tasks.add_task',login_url='no-permission')]
+
+# @method_decorator(create_decorators, name="dispatch")
+class CreateTask(ContextMixin,LoginRequiredMixin,PermissionRequiredMixin,View):
+    
+    permission_required='tasks.add_task'
+    login_url='sign-in'
+    template_name="task_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["task_form"]=kwargs.get("task_from",TaskModelForm())
+        context["task_detail_form"]=kwargs.get("task_detail_form",TaskDetailModelForm)
+        return context
+
+    def get(self,request,*args, **kwargs):
+         task_form = TaskModelForm()  # For GET
+         task_detail_form = TaskDetailModelForm()
+         context=self.get_context_data()
+         return render(request,self.template_name,context)
+
+    def post(self,request,*args, **kwargs):
+        task_form = TaskModelForm(request.POST)
+        task_detail_form = TaskDetailModelForm(request.POST,request.FILES)
+        if task_form.is_valid() and task_detail_form.is_valid():
+
+            '''For Model Form Data'''
+            task = task_form.save()
+            task_detail = task_detail_form.save(commit=False)
+            task_detail.task = task
+            task_detail.save()
+
+            messages.success(request, 'Task created successfully')
+            context=self.get_context_data(task_form=task_form,task_detail_form=task_detail_form)
+            return render(request,self.template_name,context)
+        
+
+
 @login_required
 @permission_required('tasks.change_task',login_url='no-permission')
 def update_task(request, id):
@@ -127,6 +183,19 @@ def view_task(request):
     projects=Project.objects.annotate(
         num_task=Count('task')).order_by('num_task')
     return render(request,'show-task.html',{"projects":projects})
+
+
+view_project_decorators=[login_required,permission_required('projects.view-task',login_url='no-permission')]
+
+@method_decorator(view_project_decorators, name="dispatch")
+class ViewProject(ListView):
+    model=Project
+    context_object_name="projects"
+    template_name="show_task.html"
+
+    def get_queryset(self):
+        queryset=Project.objects.annotate(num_task=Count('task')).order_by('num_task')
+        return queryset
 
 @login_required
 @permission_required('tasks.view_task',login_url='no-permission')
